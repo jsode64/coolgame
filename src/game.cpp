494a4e0b4bd4c4590825668020c618cd02e56de7
jdaschcode <joeldasch@gmail.com>
2025-10-08 21:21:26 +0000

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -6,7 +6,7 @@
 #include "fighters/oscar.hpp"
 
 Game::Game()
-    : stage(Stage::test()), fighters(), attacks() {
+    : stage(Stage::stage_one()), fighters(), attacks() {
     fighters.push_back(std::make_unique<Luie>(KEY_A, KEY_D, KEY_W, KEY_S));
     fighters.push_back(std::make_unique<Oscar>(KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN));
 }
@@ -17,6 +17,15 @@ void Game::update() {
     for (auto& fighter: fighters) {
         fighter->update(stage, attacks);
     }
+
+    for(auto& tile : stage.get_bodies()) {
+        tile.touch_test();
+    }
+
+    for(auto& tile : stage.get_bodies()) {
+        tile.rise_test();
+    }
+
 
     // Update attacks and remove finished ones.
     for (size_t i = 0; i < attacks.size();) {
