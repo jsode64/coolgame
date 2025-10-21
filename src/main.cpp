810909fd3df7bcfc9a765b41106fcad653bcd052
fordja@sunypoly.edu <fordja@sunypoly.edu>
2025-10-21 14:21:06 -0400

    resolving branch issues

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,12 @@
 #include "game.hpp"
 
 
+int32_t screenW;
+int32_t screenH;
 
 int main() {
     // Initialize window.
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     InitWindow(WIN_W, WIN_H, "Cool Game");
 
     // Initialize Audio
@@ -20,6 +23,35 @@ int main() {
 
     // Main game loop
     while (!WindowShouldClose()) {
+        if (IsWindowResized() && !IsWindowFullscreen())
+        {
+            screenW = GetScreenWidth();
+            screenH = GetScreenHeight();
+        }
+
+        // check for alt + enter
+ 		if (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)))
+ 		{
+            // see what display we are on right now
+ 			int display = GetCurrentMonitor();
+ 
+            
+            if (IsWindowFullscreen())
+            {
+                // if we are full screen, then go back to the windowed size
+                SetWindowSize(screenW, screenH);
+            }
+            else
+            {
+                // if we are not full screen, set the window size to match the monitor we are on
+                SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+            }
+ 
+            // toggle the state
+ 			ToggleFullscreen();
+ 		}
+
+
         game.update();
 
         // Render in here:
