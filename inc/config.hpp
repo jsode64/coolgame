81dd08d59f8ae200ce086c774c7c86724a410fdb
fordja@sunypoly.edu <fordja@sunypoly.edu>
2025-11-05 17:10:01 -0500

    window

--- inc/config.hpp
+++ inc/config.hpp
@@ -4,5 +4,7 @@
 
 static constexpr int32_t WIN_W = 800;
 static constexpr int32_t WIN_H = 448;
+static int32_t screenW;
+static int32_t screenH;
 
 static constexpr float GRAVITY = 0.5f;
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 
 int main() {
   SetTraceLogLevel(LOG_NONE);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
   InitWindow(WIN_W, WIN_H, "Cool Game");
   SetTargetFPS(60);
 
@@ -20,6 +21,34 @@ int main() {
   auto sb = LoadTexture("assets/hi.jpg");
 
   while (!WindowShouldClose()) {
+     if (IsWindowResized() && !IsWindowFullscreen())
+        {
+            screenW = GetScreenWidth();
+            screenH = GetScreenHeight();
+        }
+
+        // check for alt + enter
+ 		if (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)))
+ 		{
+            // see what display we are on right now
+ 			int display = GetCurrentMonitor();
+ 
+            
+            if (IsWindowFullscreen())
+            {
+                // if we are full screen, then go back to the windowed size
+                SetWindowSize(screenW, screenH);
+            }
+            else
+            {
+                // if we are not full screen, set the window size to match the monitor we are on
+                SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+            }
+ 
+            // toggle the state
+ 			ToggleFullscreen();
+ 		}
+
     game.update();
 
     // Render in here:
